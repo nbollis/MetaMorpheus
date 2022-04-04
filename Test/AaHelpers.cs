a446cf3760e392e8ee34cd3885c27b629d4e8c17
Nic Bollis <nbollis@comcast.net>
2022-04-04 10:28:36 -0600

    Added helper method which counts the number of mods

---
+++ Test/AaHelpers.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestFixture]
+
+    internal static class AaHelpers
+    {
+
+        [Test]
+        public static void FindNumberOfMods()
+        {
+            int count = 0;
+            string filepath = @"C:\Users\Nic\source\repos\MetaMorpheus\EngineLayer\Mods\Mods.txt";
+            foreach (var line in File.ReadAllLines(filepath))
+            {
+                if (line.StartsWith("ID"))
+                    count++;
+            }
+
+            Assert.AreEqual(count, 0);
+        }
+
+    }
+}
