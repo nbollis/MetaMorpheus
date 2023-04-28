f6dd905254bcf0b23084cf89dbf3703657ce6152
nbollis <nbollis@comcast.net>
2023-04-28 16:13:56 -0500

    started dnb sstuff

---
+++ Test/AveragingPaper/Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test.AveragingPaper
+{
+    internal static class DatabaseConstants
+    {
+    }
+
+    [TestFixture]
+    public static class TestDB
+    {
+        public static string DatabasePath = @"B:\Users\Nic\ScanAveraging\Averaging.sql";
+        [Test]
+        public static void TESTNAME()
+        {
+            var connection = new SQLiteConnection($"Data Source={DatabasePath}");
+            connection.Open();
+            
+
+        }
+    }
+}
