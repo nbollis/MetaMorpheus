bce6f3d9418227b4d4cd7f848650d04c6d0002ee
nbollis <nbollis@comcast.net>
2023-07-27 10:28:11 -0500

    Histogram

--- MetaMorpheus/Test/AATests.cs
+++ MetaMorpheus/Test/AATests.cs
@@ -5,9 +5,12 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Easy.Common.Interfaces;
 using EngineLayer;
+using EngineLayer.HistogramAnalysis;
 using MathNet.Numerics.Statistics;
 using NUnit.Framework;
+using OxyPlot.Series;
 
 namespace Test
 {
@@ -16,9 +19,12 @@ namespace Test
     {
 
         public static string SearchDirectoryPath = @"D:\Projects\Top Down MetaMorpheus\NB_Replicate_1.0.2\WithoutLibrary\Search";
+        public static string SearchDirectoryPath12 = @"D:\Projects\Top Down MetaMorpheus\NB_Replicate_1.0.2\WithoutLibrary\Search_12ppmPrecursorTolerance";
 
         public static string CaliDirectorPath =
             @"D:\Projects\Top Down MetaMorpheus\NB_Replicate_1.0.2\WithoutLibrary\CaliSearch";
+        public static string CaliAvgDirectorPath =
+            @"D:\Projects\Top Down MetaMorpheus\NB_Replicate_1.0.2\WithoutLibrary\CaliAvg_Search";
 
         public static string VariableDirectoryPath =
             @"D:\Projects\Top Down MetaMorpheus\NB_Replicate_1.0.2\WithoutLibrary\CaliAvg_VarPhoAce_Search";
@@ -56,30 +62,24 @@ namespace Test
             return files.First(p => p.Contains("AllProteoforms.psmtsv"));
         }
 
-        public record struct HistogramBin(double min, double max, int count);
-        private static List<HistogramBin> GenerateHistogram(double binWidth, List<double> values)
+        private static List<PsmFromTsv> GetFilteredPsms(List<PsmFromTsv> unfilteredPsms)
         {
-            var min = values.Min();
-            var max = values.Max();
-            List<HistogramBin> bins = new();
-            for (double i = min-binWidth; i < max + binWidth; i+=binWidth)
-            {
-                HistogramBin bin = new(i, i + binWidth, 0);
-                var count = values.Count(p => p >= bin.min && p < bin.max);
-                bin.count = count;
-                bins.Add(bin);
-            }
-
-            return bins;
+            return unfilteredPsms.Where(p =>
+                p.DecoyContamTarget == "T" &&
+                //p.Notch == "0" &&
+                !p.MassDiffPpm.Contains("|") &&
+                (p.FileNameWithoutExtension.Contains("FXN5")
+                 || p.FileNameWithoutExtension.Contains("FXN6")
+                 || p.FileNameWithoutExtension.Contains("FXN7")
+                 || p.FileNameWithoutExtension.Contains("FXN8")) &&
+                p.QValue <= 0.01)
+                .ToList();
         }
 
-        public static void ExportHistogram(this List<HistogramBin> histogramBinLines, string outPath)
+        private static List<double>  GetAllFragmentIonPpmErrors(List<PsmFromTsv> psms)
         {
-            using (var sw = new StreamWriter(File.Create(outPath)))
-            {
-                sw.WriteLine("min,max,avg,count");
-                histogramBinLines.ForEach(p => sw.WriteLine($"{p.min},{p.max},{(p.min + p.max) / 2},{p.count}"));
-            }
+            return psms.SelectMany(p => p.MatchedIons.Select(m => m.MassErrorPpm))
+                .ToList();
         }
 
         #endregion
@@ -109,6 +109,164 @@ namespace Test
 
         }
 
-        
+
+        [Test]
+        public static void PrecursorHist()
+        {
+            double binSize = 0.1;
+            double min = -20;
+            double max = 20;
+
+
+            var filteredSearch12 =
+                GetFilteredPsms(PsmTsvReader.ReadTsv(GetPsmsPath(SearchDirectoryPath12), out List<string> warnings));
+            var searchGram = new Histogram(filteredSearch12.Select(p => double.Parse(p.MassDiffPpm)), binSize, min, max, "Search");
+            var filteredCalibSearch =
+                GetFilteredPsms(PsmTsvReader.ReadTsv(GetPsmsPath(CaliDirectorPath), out warnings));
+            var calibGram = new Histogram(filteredCalibSearch.Select(p => double.Parse(p.MassDiffPpm)), binSize, min, max, "Calibrated");
+            var filteredCalibAvgSearch =
+                GetFilteredPsms(PsmTsvReader.ReadTsv(GetPsmsPath(CaliAvgDirectorPath), out warnings));
+            var avgGram = new Histogram(filteredCalibAvgSearch.Select(p => double.Parse(p.MassDiffPpm)), binSize, min, max, "Calibrated-Averaged");
+
+            string outpath = @"D:\Projects\Top Down MetaMorpheus\NB_Replicate_1.0.2\WithoutLibrary\Grams\Targets58FDR.csv";
+            new List<Histogram> { searchGram, calibGram, avgGram }.ExportMultipleGrams(outpath);
+        }
+
+        [Test]
+        public static void FragmentIonHist()
+        {
+            double binSize = 0.1;
+            double min = -20;
+            double max = 20;
+
+
+            var filteredSearch12 =
+                GetFilteredPsms(PsmTsvReader.ReadTsv(GetPsmsPath(SearchDirectoryPath12), out List<string> warnings));
+            var searchGram = new Histogram(GetAllFragmentIonPpmErrors(filteredSearch12), binSize, min, max, "Search");
+            var filteredCalibSearch =
+                GetFilteredPsms(PsmTsvReader.ReadTsv(GetPsmsPath(CaliDirectorPath), out warnings));
+            var calibGram = new Histogram(GetAllFragmentIonPpmErrors(filteredCalibSearch), binSize, min, max, "Calibrated");
+            var filteredCalibAvgSearch =
+                GetFilteredPsms(PsmTsvReader.ReadTsv(GetPsmsPath(CaliAvgDirectorPath), out warnings));
+            var avgGram = new Histogram(GetAllFragmentIonPpmErrors(filteredCalibAvgSearch), binSize, min, max, "Calibrated-Averaged");
+
+            string outpath = @"D:\Projects\Top Down MetaMorpheus\NB_Replicate_1.0.2\WithoutLibrary\Grams\Frag_Targets58FDR.csv";
+            new List<Histogram> { searchGram, calibGram, avgGram }.ExportMultipleGrams(outpath);
+        }
+    }
+
+    public class HistogramBin
+    {
+        public double Start;
+        public double End;
+        public double Count;
+
+        public HistogramBin(double start, double end, double count)
+        {
+            Start = start;
+            End = end;
+            Count = count;
+        }
+
+        public override string ToString()
+        {
+            return $"{End},{Count}";
+        }
+    };
+
+    public class Histogram
+    {
+        private double min;
+        private double max;
+
+        public double[] Values { get; init; }
+
+        public double BinSize { get; init; }
+
+        public HistogramBin[] Bins { get; private set; }
+
+        public string Identifier { get; init; }
+
+        public double AverageValue { get; init; }
+        public double StandardDeviationValue { get; init; }
+
+        public Histogram(IEnumerable<double> values, double binSize, double? min = null, double? max = null, string identifier = "")
+        {
+            Values = values.OrderBy(p => p).ToArray();
+            BinSize = binSize;
+            this.min = min ?? Values.Min();
+            this.max = max ?? Values.Max() + BinSize;
+            Identifier = identifier;
+            AverageValue = Values.Average();
+            StandardDeviationValue = Values.StandardDeviation();
+
+            DoTheHistingAndGraming();
+        }
+
+        private void DoTheHistingAndGraming()
+        {
+            var bins = new List<HistogramBin>();
+            // create bins
+            for (double i = min; i < max; i += BinSize)
+            {
+                bins.Add(new HistogramBin(i, i + BinSize, 0));
+            }
+            bins.Add(new HistogramBin(max, 100 * max, 0));
+
+            // populate bins
+            foreach (var val in Values)
+            {
+                var binOfInterest = bins.First(bin => val >= bin.Start && val < bin.End);
+                binOfInterest.Count++;
+            }
+
+            Bins = bins.ToArray();
+        }
+
+    }
+
+    public static class HistogramExtensions
+    {
+        public static void ExportHistogram(this Histogram histogram, string outPath)
+        {
+            using (var sw = new StreamWriter(File.Create(outPath)))
+            {
+                sw.WriteLine("Bin,Frequency");
+                foreach (var bin in histogram.Bins)
+                {
+                    sw.Write(bin);
+                }
+            }
+        }
+
+        public static void ExportMultipleGrams(this List<Histogram> histograms, string outPath)
+        {
+            if (histograms.Select(p => p.Bins.Length).Distinct().Skip(1).Any())
+                throw new ArgumentException("All histograms must have same number of bins");
+
+            var identifiers = string.Join(',', histograms.Select(p => p.Identifier)) + ","
+                              + string.Join(',', histograms.Select(p => p.AverageValue)) + ","
+                              + string.Join(',', histograms.Select(p => p.StandardDeviationValue));
+            var lineValues = new double[histograms.First().Bins.Length][];
+
+            // get values in jagged array ready for output
+            for (int binIndex = 0; binIndex < histograms.First().Bins.Length; binIndex++)
+            {
+                lineValues[binIndex] = new double[histograms.Count];
+                for (int histogramIndex = 0; histogramIndex < histograms.Count; histogramIndex++)
+                {
+                    lineValues[binIndex][histogramIndex] = histograms[histogramIndex].Bins[binIndex].Count;
+                }
+            }
+            
+            using (var sw = new StreamWriter(File.Create(outPath)))
+            {
+                sw.WriteLine($"Bin,{identifiers}");
+                for (int i = 0; i < lineValues.Length; i++)
+                {
+                    sw.WriteLine($"{histograms.First().Bins[i].End},{string.Join(',', lineValues[i])}");
+                }
+            }
+        }
     }
 }
