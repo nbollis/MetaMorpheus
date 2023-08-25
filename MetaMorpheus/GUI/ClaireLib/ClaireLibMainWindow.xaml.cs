7cbce2592748210333e1c068ecb8ef3542300c3a
nbollis <nbollis@comcast.net>
2023-08-25 10:13:05 -0500

    Moved ClaireLib to Proper Folder

--- MetaMorpheus/GUI/ClaireLib/ClaireLibMainWindow.xaml.cs
+++ MetaMorpheus/GUI/ClaireLib/ClaireLibMainWindow.xaml.cs
@@ -1,22 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Drawing.Imaging;
-using System.IO;
-using Easy.Common.Extensions;
+﻿using System.Windows;
 using UsefulProteomicsDatabases;
-using Path = System.IO.Path;
 
 namespace MetaMorpheusGUI
 {
