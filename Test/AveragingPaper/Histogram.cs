b32a2d74298ddab7123475ac9f9fce8373e219c8
nbollis <nbollis@comcast.net>
2023-08-31 16:12:52 -0500

    did the histogram stuff for averaging revisions

---
+++ Test/AveragingPaper/Histogram.cs
@@ -0,0 +1,141 @@
+﻿using MathNet.Numerics.Statistics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plotly.NET.TraceObjects;
+
+namespace Test
+{
+    public class Histogram
+    {
+        private double min;
+        private double max;
+
+        public List<double> Values { get; init; }
+
+        public double BinSize { get; init; }
+
+        public HistogramBin[] Bins { get; private set; }
+
+        public string Identifier { get; init; }
+
+        public double AverageValue { get; init; }
+        public double StandardDeviationValue { get; init; }
+
+        public Histogram(IEnumerable<double> values, double binSize, double? min = null, double? max = null, string identifier = "")
+        {
+            Values = values.Where(p => !double.IsNaN(p)).ToList();
+            BinSize = binSize;
+            this.min = min ?? Values.Min();
+            this.max = max ?? Values.Max() + BinSize;
+            Identifier = identifier;
+            AverageValue = Values.Average();
+            StandardDeviationValue = Values.StandardDeviation();
+
+            DoTheHistingAndGraming();
+        }
+
+        private void DoTheHistingAndGraming()
+        {
+            var bins = new List<HistogramBin>();
+            // create bins
+            for (double i = min; i < max; i += BinSize)
+            {
+                bins.Add(new HistogramBin(i, i + BinSize, 0));
+            }
+            //bins.Add(new HistogramBin(max, 100 * max, 0));
+
+            // populate bins
+            foreach (var val in Values)
+            {
+                var binOfInterest = bins.First(bin => val >= bin.Start && val < bin.End);
+                binOfInterest.Count++;
+            }
+
+            Bins = bins.ToArray();
+        }
+
+    }
+
+    public class HistogramBin
+    {
+        public double Start;
+        public double End;
+        public double Count;
+
+        public HistogramBin(double start, double end, double count)
+        {
+            Start = start;
+            End = end;
+            Count = count;
+        }
+
+        public override string ToString()
+        {
+            return $"{End},{Count}";
+        }
+    };
+
+
+    public static class HistogramExtensions
+    {
+        public static void ExportHistogram(this Histogram histogram, string outPath, bool withPercent = false)
+        {
+            using (var sw = new StreamWriter(File.Create(outPath)))
+            {
+                if (withPercent)
+                {
+                    sw.WriteLine("Bin,Frequency,Percent");
+                    var total = histogram.Bins.Sum(p => p.Count);
+                    foreach (var bin in histogram.Bins)
+                    {
+                        sw.WriteLine($"{bin},{bin.Count / total * 100}");
+                    }
+                }
+                else
+                {
+                    sw.WriteLine("Bin,Frequency");
+                    foreach (var bin in histogram.Bins)
+                    {
+                        sw.WriteLine(bin);
+                    }
+                }
+
+                
+            }
+        }
+
+        public static void ExportMultipleGrams(this List<Histogram> histograms, string outPath)
+        {
+            if (histograms.Select(p => p.Bins.Length).Distinct().Skip(1).Any())
+                throw new ArgumentException("All histograms must have same number of bins");
+
+            var identifiers = string.Join(',', histograms.Select(p => p.Identifier)) + ","
+                + string.Join(',', histograms.Select(p => p.AverageValue)) + ","
+                + string.Join(',', histograms.Select(p => p.StandardDeviationValue));
+            var lineValues = new double[histograms.First().Bins.Length][];
+
+            // get values in jagged array ready for output
+            for (int binIndex = 0; binIndex < histograms.First().Bins.Length; binIndex++)
+            {
+                lineValues[binIndex] = new double[histograms.Count];
+                for (int histogramIndex = 0; histogramIndex < histograms.Count; histogramIndex++)
+                {
+                    lineValues[binIndex][histogramIndex] = histograms[histogramIndex].Bins[binIndex].Count;
+                }
+            }
+
+            using (var sw = new StreamWriter(File.Create(outPath)))
+            {
+                sw.WriteLine($"Bin,{identifiers}");
+                for (int i = 0; i < lineValues.Length; i++)
+                {
+                    sw.WriteLine($"{histograms.First().Bins[i].End},{string.Join(',', lineValues[i])}");
+                }
+            }
+        }
+    }
+}
---
+++ Test/AveragingPaper/MWProcessing.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EngineLayer;
+using NUnit.Framework;
+using Proteomics;
+using Proteomics.ProteolyticDigestion;
+using Readers;
+using UsefulProteomicsDatabases;
+
+namespace Test
+{
+    [TestFixture]
+    internal class MwProcessing
+    {
+        public static string CalibCentroidDirectory1 =
+            @"D:\Projects\SpectralAveraging\PaperTestOutputs\JurkatTopDown_DeconvolutionAnalysis\Rep1CalibCentroid";
+        public static string CalibAverageCentroidDirectory1 =
+            @"D:\Projects\SpectralAveraging\PaperTestOutputs\JurkatTopDown_DeconvolutionAnalysis\Rep1CalibAverageCentroid";
+        public static string CalibCentroidDirectory2 =
+            @"D:\Projects\SpectralAveraging\PaperTestOutputs\JurkatTopDown_DeconvolutionAnalysis\Rep2CalibCentroided";
+        public static string CalibAverageCentroidDirectory2 =
+            @"D:\Projects\SpectralAveraging\PaperTestOutputs\JurkatTopDown_DeconvolutionAnalysis\Rep2CalibAverageCentroid";
+
+        private Dictionary<string, Modification> _unknownModifications;
+
+
+        private static string[] GetFeatureFiles(string directoryPath)
+        {
+            return Directory.GetFiles(directoryPath).Where(p => p.EndsWith("ms1.feature")).OrderBy(p => p).ToArray();
+        }
+
+
+        [Test]
+        public void FeatureMassHistogram()
+        {
+            var directories = new List<string> { CalibCentroidDirectory2, CalibAverageCentroidDirectory2 };
+
+            foreach (var directory in directories)
+            {
+                var topFDFeatureFiles = GetFeatureFiles(Path.Combine(directory, "TopFD"));
+                var flashFeatureFiles = GetFeatureFiles(Path.Combine(directory, "FlashDeconv"));
+
+
+                // topFD
+                List<double> topFdMasses = new List<double>();
+                foreach (var featureFile in topFDFeatureFiles)
+                {
+                    var file = FileReader.ReadFile<Readers.Ms1FeatureFile>(featureFile);
+                    topFdMasses.AddRange(file.Results.Select(p => Math.Log10(p.Mass)));
+                }
+                var topFDHist = new Histogram(topFdMasses, 0.01);
+                topFDHist.ExportHistogram(Path.Combine(directory, "TopFDLogHist.csv"), true);
+
+                // FlashDeconv
+
+                List<double> flashMasses = new List<double>();
+                foreach (var featureFile in flashFeatureFiles)
+                {
+                    var file = FileReader.ReadFile<Readers.Ms1FeatureFile>(featureFile);
+                    flashMasses.AddRange(file.Results.Select(p => Math.Log10(p.Mass)));
+                }
+                var flashHist = new Histogram(flashMasses, 0.01);
+                flashHist.ExportHistogram(Path.Combine(directory, "FlashLogHist.csv"), true);
+            }
+        }
+
+        [Test]
+        public void PsmMassHistogram()
+        {
+
+
+            var directories = new List<string> { /*CalibCentroidDirectory2,*/ CalibAverageCentroidDirectory2 };
+            List<double> masses = new List<double>();
+            foreach (var directory in directories)
+            {
+                //var path = Path.Combine(directory, "MMSearch", "Task2-SearchTask", "AllPSMs.psmtsv");
+                var path =
+                    @"B:\Users\Katie\Jurkat proteoforms paper\Jurkat28FXNs\2017-07-05-12-56-46\Task3Search\allPSMs_5ppmAroundZero.psmtsv";
+                var psms = PsmTsvReader.ReadTsv(path, out List<string> warnings).Where(p => p.QValue <= 0.01);
+
+                masses = psms.Select(p => Math.Log10(p.PrecursorMass)).ToList();
+                var hist = new Histogram(masses, 0.01);
+                hist.ExportHistogram(Path.Combine(directory, "BUPsmLogHist.csv"), true);
+            }
+        }
+
+        [Test]
+        public void ProteinsMassHistogram()
+        {
+
+
+            var directories = new List<string> { /*CalibCentroidDirectory2,*/ CalibAverageCentroidDirectory2 };
+            foreach (var directory in directories)
+            {
+                //var path = Path.Combine(directory, "MMSearch", "Task2-SearchTask", "AllPSMs.psmtsv");
+                var path =
+                    @"D:\Projects\Top Down MetaMorpheus\RawSpectra\uniprot-proteome_UP000005640_HumanRevPlusUnrev_012422.xml";
+                _unknownModifications = new Dictionary<string, Modification>();
+                var proteins = ProteinDbLoader.LoadProteinXML(path, true, DecoyType.None, GlobalVariables.AllModsKnown,
+                    false, new List<string>(), out _unknownModifications);
+
+                var peps = proteins.SelectMany(p =>
+                    p.Digest(new DigestionParams("top-down"), new List<Modification>(), new List<Modification>()))
+                    .ToList();
+
+                var masses = peps.Select(p => Math.Log10(p.MonoisotopicMass)).Where(p => p != Double.NaN);
+
+                var hist = new Histogram(masses, 0.01);
+                hist.ExportHistogram(Path.Combine(directory, "DBLogHist.csv"), true);
+            }
+        }
+    }
+}
