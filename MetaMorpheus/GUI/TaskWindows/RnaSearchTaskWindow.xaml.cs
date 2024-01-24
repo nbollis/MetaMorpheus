97f99c73468aa95afc10f46b7ecd4381b0d18806
nbollis <nbollis@comcast.net>
2024-01-24 12:47:27 -0600

    Built base search task window

--- MetaMorpheus/GUI/TaskWindows/RnaSearchTaskWindow.xaml.cs
+++ MetaMorpheus/GUI/TaskWindows/RnaSearchTaskWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,13 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using EngineLayer;
 using GuiFunctions;
 using GuiFunctions.Transcriptomics;
+using MassSpectrometry;
+using MzLibUtil;
+using TaskLayer;
+using Transcriptomics.Digestion;
 
 namespace MetaMorpheusGUI
 {
@@ -21,10 +27,114 @@ namespace MetaMorpheusGUI
     /// </summary>
     public partial class RnaSearchTaskWindow : Window
     {
-        public RnaSearchTaskWindow()
+        internal RnaSearchParametersViewModel TheTaskViewModel { get; private set; }
+        internal RnaSearchTask TheTask { get; private set; }
+        private readonly ObservableCollection<ModTypeForTreeViewModel> FixedModTypeForTreeViewObservableCollection = new ObservableCollection<ModTypeForTreeViewModel>();
+        private readonly ObservableCollection<ModTypeForTreeViewModel> VariableModTypeForTreeViewObservableCollection = new ObservableCollection<ModTypeForTreeViewModel>();
+        public RnaSearchTaskWindow(RnaSearchTask task = null)
         {
             InitializeComponent();
-            DataContext = new RnaSearchParametersViewModel();
+            TheTask = task ?? new RnaSearchTask();
+            PopulateChoices();
+            UpdateFieldsFromTask(TheTask);
+            DataContext = TheTaskViewModel;
+        }
+
+        private void UpdateFieldsFromTask(RnaSearchTask task)
+        {
+            TheTaskViewModel = new RnaSearchParametersViewModel()
+            {
+                digestionParams = task.CommonParameters.DigestionParams as RnaDigestionParams,
+                searchParams = task.SearchParameters,
+            };
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void PopulateChoices()
+        {
+            foreach (var hm in GlobalVariables.AllModsKnown.Where(b => b.ValidModification == true).GroupBy(b => b.ModificationType))
+            {
+                var theModType = new ModTypeForTreeViewModel(hm.Key, false);
+                FixedModTypeForTreeViewObservableCollection.Add(theModType);
+                foreach (var uah in hm)
+                {
+                    theModType.Children.Add(new ModForTreeViewModel(uah.ToString(), false, uah.IdWithMotif, false, theModType));
+                }
+            }
+            FixedModsTreeView.DataContext = FixedModTypeForTreeViewObservableCollection;
+
+            foreach (var hm in GlobalVariables.AllModsKnown.Where(b => b.ValidModification == true).GroupBy(b => b.ModificationType))
+            {
+                var theModType = new ModTypeForTreeViewModel(hm.Key, false);
+                VariableModTypeForTreeViewObservableCollection.Add(theModType);
+                foreach (var uah in hm)
+                {
+                    theModType.Children.Add(new ModForTreeViewModel(uah.ToString(), false, uah.IdWithMotif, false, theModType));
+                }
+            }
+            VariableModsTreeView.DataContext = VariableModTypeForTreeViewObservableCollection;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            RnaSearchParameters searchParams = TheTaskViewModel.searchParams;
+            RnaDigestionParams digestionParams = TheTaskViewModel.digestionParams;
+            try
+            {
+                Rnase rnase = RnaseDictionary.Dictionary[TheTaskViewModel.SelectedRnase];
+                digestionParams.GetType().GetProperty("Rnase")?.SetValue(digestionParams, rnase);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Rnase loading error with message: {ex.Message}");
+            }
+
+            var listOfModsVariable = new List<(string, string)>();
+            foreach (var heh in VariableModTypeForTreeViewObservableCollection)
+            {
+                listOfModsVariable.AddRange(heh.Children.Where(b => b.Use).Select(b => (b.Parent.DisplayName, b.ModName)));
+            }
+
+            var listOfModsFixed = new List<(string, string)>();
+            foreach (var heh in FixedModTypeForTreeViewObservableCollection)
+            {
+                listOfModsFixed.AddRange(heh.Children.Where(b => b.Use).Select(b => (b.Parent.DisplayName, b.ModName)));
+            }
+
+            CommonParameters commonParams = new
+            (
+                taskDescriptor: OutputFileNameTextBox.Text != "" ? OutputFileNameTextBox.Text : "RnaSearchTask",
+                maxThreadsToUsePerFile: TheTaskViewModel.MaxThreads,
+                digestionParams: digestionParams,
+                listOfModsVariable: listOfModsVariable,
+                listOfModsFixed: listOfModsFixed,
+                dissociationType: TheTaskViewModel.DissociationType,
+                deconvolutionMaxAssumedChargeState: TheTaskViewModel.MaxAssumedChargeState,
+                deconvolutionMassTolerance: new PpmTolerance(TheTaskViewModel.DeconvolutionMassTolerance),
+                precursorMassTolerance: new PpmTolerance(TheTaskViewModel.PrecursorMassTolerance),
+                productMassTolerance: new PpmTolerance(TheTaskViewModel.ProductMassTolerance),
+                doPrecursorDeconvolution: TheTaskViewModel.DoPrecursorDeconvolution,
+                useProvidedPrecursorInfo: TheTaskViewModel.UseProvidedPrecursorInfo,
+                qValueThreshold: TheTaskViewModel.QValueCutoff,
+                scoreCutoff: TheTaskViewModel.ScoreCutoff,
+                deconvolutionIntensityRatio: 3
+            );
+            TheTask = new RnaSearchTask()
+            {
+                CommonParameters = commonParams,
+                SearchParameters = searchParams,
+            };
+
+            DialogResult = true;
+        }
+
+        private void SaveAsDefault_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- MetaMorpheus/GuiFunctions/Transcriptomics/RnaSearchParametersViewModel.cs
+++ MetaMorpheus/GuiFunctions/Transcriptomics/RnaSearchParametersViewModel.cs
@@ -18,43 +18,45 @@ namespace GuiFunctions.Transcriptomics
         public RnaSearchParametersViewModel()
         {
             searchParams = new RnaSearchParameters();
-            commonParams = new CommonParameters(digestionParams: new RnaDigestionParams(fragmentationTerminus: FragmentationTerminus.Both));
+            digestionParams = new RnaDigestionParams(fragmentationTerminus: FragmentationTerminus.Both);
             selectedRnase = RnaseDictionary.Dictionary.Values.First().Name;
+            MaxAssumedChargeState = -12;
+
         }
 
-        private RnaSearchParameters searchParams { get; set; }
-        private CommonParameters commonParams { get; set; }
+        public RnaSearchParameters searchParams { get; set; }
+        public RnaDigestionParams digestionParams { get; set; }
 
         #region Digestion Params
 
         public int MinLength
         {
-            get => commonParams.DigestionParams.MinLength;
-            set { commonParams.DigestionParams.MinLength = value; OnPropertyChanged(nameof(MinLength)); }
+            get => digestionParams.MinLength;
+            set { digestionParams.MinLength = value; OnPropertyChanged(nameof(MinLength)); }
         }
 
         public int MaxLength
         {
-            get => commonParams.DigestionParams.MaxLength;
-            set { commonParams.DigestionParams.MaxLength = value; OnPropertyChanged(nameof(MaxLength)); }
+            get => digestionParams.MaxLength;
+            set { digestionParams.MaxLength = value; OnPropertyChanged(nameof(MaxLength)); }
         }
 
         public int MaxMissedCleavages
         {
-            get => commonParams.DigestionParams.MaxMissedCleavages;
-            set { commonParams.DigestionParams.MaxMissedCleavages = value; OnPropertyChanged(nameof(MaxMissedCleavages)); }
+            get => digestionParams.MaxMissedCleavages;
+            set { digestionParams.MaxMissedCleavages = value; OnPropertyChanged(nameof(MaxMissedCleavages)); }
         }
 
         public int MaxMods
         {
-            get => commonParams.DigestionParams.MaxMods;
-            set { commonParams.DigestionParams.MaxMods = value; OnPropertyChanged(nameof(MaxMods)); }
+            get => digestionParams.MaxMods;
+            set { digestionParams.MaxMods = value; OnPropertyChanged(nameof(MaxMods)); }
         }
 
         public int MaxModificationIsoforms
         {
-            get => commonParams.DigestionParams.MaxModificationIsoforms;
-            set { commonParams.DigestionParams.MaxModificationIsoforms = value; OnPropertyChanged(nameof(MaxModificationIsoforms)); }
+            get => digestionParams.MaxModificationIsoforms;
+            set { digestionParams.MaxModificationIsoforms = value; OnPropertyChanged(nameof(MaxModificationIsoforms)); }
         }
 
         public ObservableCollection<string> AllRnases { get; set; } =
@@ -129,6 +131,10 @@ namespace GuiFunctions.Transcriptomics
             set { searchParams.DecoyType = value; OnPropertyChanged(nameof(DecoyType)); }
         }
 
+
+
+
+
         public ObservableCollection<DissociationType> AllDissociationTypes { get; set; } =
             new ObservableCollection<DissociationType>(Enum.GetValues(typeof(DissociationType)).Cast<DissociationType>());
 
@@ -140,31 +146,13 @@ namespace GuiFunctions.Transcriptomics
         }
 
         private int maxThreads { get; set; }
-
         public int MaxThreads
         {
             get => maxThreads;
             set { maxThreads = value; OnPropertyChanged(nameof(MaxThreads)); }
         }
 
-        private List<(string, string)> variableMods { get; set; }
-
-        public List<(string, string)> VariableMods
-        {
-            get => variableMods;
-            set { variableMods = value; OnPropertyChanged(nameof(VariableMods)); }
-        }
-
-        private List<(string, string)> fixedMods { get; set; }
-
-        public List<(string, string)> FixedMods
-        {
-            get => fixedMods;
-            set { fixedMods = value; OnPropertyChanged(nameof(FixedMods)); }
-        }
-
         private bool doPrecursorDeconvolution { get; set; }
-
         public bool DoPrecursorDeconvolution
         {
             get => doPrecursorDeconvolution;
@@ -172,31 +160,28 @@ namespace GuiFunctions.Transcriptomics
         }
 
         private bool useProvidedPrecursorInfo { get; set; }
-
         public bool UseProvidedPrecursorInfo
         {
             get => useProvidedPrecursorInfo;
             set { useProvidedPrecursorInfo = value; OnPropertyChanged(nameof(UseProvidedPrecursorInfo)); }
         }
 
-        private int minAssumedChargeState { get; set; }
-
-        public int MinAssumedChargeState
-        {
-            get => minAssumedChargeState;
-            set { minAssumedChargeState = value; OnPropertyChanged(nameof(MinAssumedChargeState)); }
-        }
 
         private int maxAssumedChargeState { get; set; }
-
         public int MaxAssumedChargeState
         {
             get => maxAssumedChargeState;
             set { maxAssumedChargeState = value; OnPropertyChanged(nameof(MaxAssumedChargeState)); }
         }
 
-        private int precursorMassTolerance { get; set; }
+        private int deconvolutionMassTolerance { get; set; }
+        public int DeconvolutionMassTolerance
+        {
+            get => deconvolutionMassTolerance;
+            set { deconvolutionMassTolerance = value; OnPropertyChanged(nameof(DeconvolutionMassTolerance)); }
+        }
 
+        private int precursorMassTolerance { get; set; }
         public int PrecursorMassTolerance
         {
             get => precursorMassTolerance;
@@ -204,7 +189,6 @@ namespace GuiFunctions.Transcriptomics
         }
 
         private int productMassTolerance { get; set; }
-
         public int ProductMassTolerance
         {
             get => productMassTolerance;
@@ -212,7 +196,6 @@ namespace GuiFunctions.Transcriptomics
         }
 
         private double qValueCutoff { get; set; }
-
         public double QValueCutoff
         {
             get => qValueCutoff;
@@ -220,7 +203,6 @@ namespace GuiFunctions.Transcriptomics
         }
 
         private double scoreCutoff { get; set; }
-
         public double ScoreCutoff
         {
             get => scoreCutoff;
