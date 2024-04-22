731c073ac3c7bac5b1365ea88ad4c2c81a0f88c3
nbollis <nbollis@comcast.net>
2024-04-22 15:14:19 -0500

    minor restructure

--- Test/ChimeraPaper/ResultFiles/BulkResults/MsFraggerResult.cs
+++ Test/ChimeraPaper/ResultFiles/BulkResults/MsFraggerResult.cs
@@ -248,4 +248,30 @@ namespace Test.ChimeraPaper.ResultFiles
             return bulkComparisonFile;
         }
     }
+
+    public class MsFraggerIndividualFileResult
+    {
+        public string DirectoryPath { get; set; }
+
+        private string _psmPath;
+        private MsFraggerPsmFile _psmFile;
+        public MsFraggerPsmFile PsmFile => _psmFile ??= new MsFraggerPsmFile(_psmPath);
+
+        private string _peptidePath;
+        private MsFraggerPeptideFile _peptideFile;
+        public MsFraggerPeptideFile PeptideFile => _peptideFile ??= new MsFraggerPeptideFile(_peptidePath);
+
+        private string _proteinPath;
+        private MsFraggerProteinFile _proteinFile;
+        public MsFraggerProteinFile ProteinFile => _proteinFile ??= new MsFraggerProteinFile(_proteinPath);
+
+
+        public MsFraggerIndividualFileResult(string directoryPath)
+        {
+            DirectoryPath = directoryPath;
+            _psmPath = System.IO.Path.Combine(DirectoryPath, "psm.tsv");
+            _peptidePath = System.IO.Path.Combine(DirectoryPath, "peptide.tsv");
+            _proteinPath = System.IO.Path.Combine(DirectoryPath, "protein.tsv");
+        }
+    }
 }
---
+++ Test/ChimeraPaper/ResultFiles/BulkResults/MsPathFinderTResults.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.ChimeraPaper.ResultFiles
+{
+    internal class MsPathFinderTResults : BulkResult
+    {
+        public MsPathFinderTResults(string directoryPath) : base(directoryPath)
+        {
+        }
+
+        public override BulkResultCountComparisonFile IndividualFileComparison(string path = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ChimeraCountingFile CountChimericPsms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override BulkResultCountComparisonFile GetBulkResultCountComparisonFile(string path = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
