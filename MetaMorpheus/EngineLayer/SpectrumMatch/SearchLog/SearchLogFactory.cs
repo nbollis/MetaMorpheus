322daa7a2d3d8889b89e124174ae79cfb0953f5e
nbollis <nbollis@comcast.net>
2025-03-13 19:11:33 -0500

    Simplified factory

--- MetaMorpheus/EngineLayer/SpectrumMatch/SearchLog/SearchLogFactory.cs
+++ MetaMorpheus/EngineLayer/SpectrumMatch/SearchLog/SearchLogFactory.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 namespace EngineLayer.SpectrumMatch;
 
 /// <summary>
@@ -17,44 +17,15 @@ public enum SearchLogType
 /// </summary>
 internal static class SearchLogFactory
 {
-    internal static Dictionary<SearchLogType, ISearchLogFactory> AllSupportedLogs = new()
-    {
-        {SearchLogType.TopScoringOnly, new TopScoringOnlyLogFactory()},
-        {SearchLogType.KeepAllDecoyScores, new KeepNScoresLogFactory(0, uint.MaxValue)},
-        {SearchLogType.Keep7DecoyScores, new KeepNScoresLogFactory(0, 7)},
-        {SearchLogType.KeepAllTargetAndDecoyScores, new KeepNScoresLogFactory(uint.MaxValue, uint.MaxValue)},
-    };
-
-    internal static ISearchLogFactory GetSearchLogFactory(SearchLogType searchLogType)
-    {
-        return AllSupportedLogs[searchLogType];
-    }
-
     internal static SearchLog GetSearchLog(SearchLogType searchLogType, double scoreCutoff = 0)
     {
-        return GetSearchLogFactory(searchLogType).GetSearchLog(scoreCutoff);
+        return searchLogType switch
+        {
+            SearchLogType.TopScoringOnly => new TopScoringOnlySearchLog(scoreCutoff),
+            SearchLogType.KeepAllDecoyScores => new KeepNScoresSearchLog(scoreCutoff, 0, ushort.MaxValue),
+            SearchLogType.Keep7DecoyScores => new KeepNScoresSearchLog(scoreCutoff, 0, 7),
+            SearchLogType.KeepAllTargetAndDecoyScores => new KeepNScoresSearchLog(scoreCutoff, ushort.MaxValue, ushort.MaxValue),
+            _ => throw new ArgumentOutOfRangeException(nameof(searchLogType), searchLogType, null)
+        };
     }
-
-    // This region defines the concrete factories that create search logs
-    #region Concrete Factories
-
-    /// <summary>
-    /// Creates standard search log, keeps top scoring results, discards once any result outscores it. 
-    /// </summary>
-    private class TopScoringOnlyLogFactory : ISearchLogFactory
-    {
-        public SearchLog GetSearchLog(double scoreCutoff) => new TopScoringOnlySearchLog(scoreCutoff: scoreCutoff);
-    }
-
-    private class KeepNScoresLogFactory(uint targetsToKeep, uint decoysToKeep) : ISearchLogFactory
-    {
-        public SearchLog GetSearchLog(double scoreCutoff) => new KeepNScoresSearchLog(scoreCutoff: scoreCutoff, maxDecoysToKeep: decoysToKeep, maxTargetsToKeep: targetsToKeep);
-    }
-
-    #endregion
-}
-
-internal interface ISearchLogFactory
-{
-    SearchLog GetSearchLog(double scoreCutoff = 0);
 }
