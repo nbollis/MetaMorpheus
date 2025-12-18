89870f12cf818e72ceeffddcbba2b6f653fdd344
nbollis <nbollis@comcast.net>
2025-12-18 17:08:49 -0600

    Moved coverter

---
+++ MetaMorpheus/TaskLayer/ManySearchTask/Util/SemiColonDelimitedToDoubleArrayTypeConverter.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+using System.Collections.Generic;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using MzLibUtil;
+
+namespace TaskLayer.ManySearchTask.Util;
+
+public class SemiColonDelimitedToDoubleArrayTypeConverter : DefaultTypeConverter
+{
+    public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    {
+        var splits = text.Split(';');
+        var toReturn = splits.Where(p => p != "");
+        return toReturn.Select(double.Parse).ToArray();
+    }
+
+    public override string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+    {
+        var list = value as IEnumerable<double> ?? throw new MzLibException("Cannot convert input to IEnumerable<double>");
+        return string.Join(';', list);
+    }
+}
