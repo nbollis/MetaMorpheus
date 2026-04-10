a27a780a1eb8a9bb632f1047d862ed6a6b7aaf97
nbollis <nbollis@comcast.net>
2026-04-10 17:56:41 -0500

    opt(Single Threaded Streamlines Search Mode)

--- MetaMorpheus/EngineLayer/ClassicSearch/StreamlinedClassicSearchEngine.cs
+++ MetaMorpheus/EngineLayer/ClassicSearch/StreamlinedClassicSearchEngine.cs
@@ -19,17 +19,23 @@ namespace EngineLayer.ClassicSearch
     public class StreamlinedClassicSearchEngine : ClassicSearchEngine
     {
         private readonly ReaderWriterLockSlim[] Locks;
+        private bool _singleThreadMode;
 
         public StreamlinedClassicSearchEngine(SpectralMatch[] globalPsms, Ms2ScanWithSpecificMass[] arrayOfSortedMS2Scans,
             List<Modification> variableModifications, List<Modification> fixedModifications, 
             List<IBioPolymer> proteinList, MassDiffAcceptor searchMode, CommonParameters commonParameters, List<(string FileName, CommonParameters Parameters)> fileSpecificParameters, List<string> nestedIds)
             : base(globalPsms, arrayOfSortedMS2Scans, variableModifications, fixedModifications, null, null, null, proteinList, searchMode, commonParameters, fileSpecificParameters, null, nestedIds, false)
         {
-            // Create one lock for each PSM to ensure thread safety
-            Locks = new ReaderWriterLockSlim[SpectralMatches.Length];
-            for (int i = 0; i < Locks.Length; i++)
+            _singleThreadMode = CommonParameters.MaxThreadsToUsePerFile <= 1;
+
+            if (!_singleThreadMode)
             {
-                Locks[i] = new ReaderWriterLockSlim();
+                // Create one lock for each PSM to ensure thread safety
+                Locks = new ReaderWriterLockSlim[SpectralMatches.Length];
+                for (int i = 0; i < Locks.Length; i++)
+                {
+                    Locks[i] = new ReaderWriterLockSlim();
+                }
             }
         }
 
@@ -44,88 +50,99 @@ namespace EngineLayer.ClassicSearch
 
             if (Proteins.Any())
             {
-                int maxThreadsPerFile = CommonParameters.MaxThreadsToUsePerFile;
-                var proteinPartioner = Partitioner.Create(0, Proteins.Count);
 
-                Parallel.ForEach(
-                    proteinPartioner,
-                    new ParallelOptions { MaxDegreeOfParallelism = maxThreadsPerFile },
-                    (range, loopState) =>
+                Action<int, int> processProteinRange = (start, end) =>
+                {
+                    List<Product> peptideTheorProducts = new();
+                    HashSet<MatchedFragmentIon> matchedFragmentIons = new();
+                    Tolerance productTolerance = CommonParameters.ProductMassTolerance;
+                    for (int i = start; i < end; i++)
                     {
-                        List<Product> peptideTheorProducts = new();
-                        HashSet<MatchedFragmentIon> matchedFragmentIons = new();
-                        Tolerance productTolerance = CommonParameters.ProductMassTolerance;
-                        for (int i = range.Item1; i < range.Item2; i++)
+                        // Stop loop if canceled
+                        if (GlobalVariables.StopLoops) { return; }
+
+                        // digest each protein into peptides and search for each peptide in all spectra within precursor mass tolerance
+                        foreach (var specificBioPolymer in Proteins[i].Digest(CommonParameters.DigestionParams, FixedModifications, VariableModifications))
                         {
-                            // Stop loop if canceled
-                            if (GlobalVariables.StopLoops) { return; }
 
-                            // digest each protein into peptides and search for each peptide in all spectra within precursor mass tolerance
-                            foreach (var specificBioPolymer in Proteins[i].Digest(CommonParameters.DigestionParams, FixedModifications, VariableModifications))
+                            peptideTheorProducts.Clear();
+                            specificBioPolymer.Fragment(CommonParameters.DissociationType, CommonParameters.DigestionParams.FragmentationTerminus, peptideTheorProducts);
+
+                            // score each scan that has an acceptable precursor mass
+                            foreach (ScanWithIndexAndNotchInfo scan in GetAcceptableScans(specificBioPolymer.MonoisotopicMass, SearchMode))
                             {
+                                matchedFragmentIons.Clear();
+                                Ms2ScanWithSpecificMass theScan = ArrayOfSortedMS2Scans[scan.ScanIndex];
+                                int precursorCharge = theScan.PrecursorCharge;
 
-                                peptideTheorProducts.Clear();
-                                specificBioPolymer.Fragment(CommonParameters.DissociationType, CommonParameters.DigestionParams.FragmentationTerminus, peptideTheorProducts);
-
-                                // score each scan that has an acceptable precursor mass
-                                foreach (ScanWithIndexAndNotchInfo scan in GetAcceptableScans(specificBioPolymer.MonoisotopicMass, SearchMode))
+                                // Match Fragment Ions
+                                foreach (var product in peptideTheorProducts)
                                 {
-                                    matchedFragmentIons.Clear();
-                                    Ms2ScanWithSpecificMass theScan = ArrayOfSortedMS2Scans[scan.ScanIndex];
-                                    int precursorCharge = theScan.PrecursorCharge;
-
-                                    // Match Fragment Ions
-                                    foreach (var product in peptideTheorProducts)
+                                    // unknown fragment mass; this only happens rarely for sequences with unknown amino acids
+                                    if (double.IsNaN(product.NeutralMass))
                                     {
-                                        // unknown fragment mass; this only happens rarely for sequences with unknown amino acids
-                                        if (double.IsNaN(product.NeutralMass))
-                                        {
-                                            continue;
-                                        }
-
-                                        // get the closest peak in the spectrum to the theoretical peak
-                                        var closestExperimentalMass = theScan.GetClosestExperimentalIsotopicEnvelope(product.NeutralMass);
-
-                                        // is the mass error acceptable?
-                                        if (closestExperimentalMass != null
-                                            && productTolerance.Within(closestExperimentalMass.MonoisotopicMass, product.NeutralMass)
-                                            && Math.Abs(closestExperimentalMass.Charge) <= Math.Abs(precursorCharge))//TODO apply this filter before picking the envelope
-                                        {
-                                            matchedFragmentIons.Add(new MatchedFragmentIon(product, closestExperimentalMass.MonoisotopicMass.ToMz(closestExperimentalMass.Charge),
-                                                closestExperimentalMass.Peaks.First().intensity, closestExperimentalMass.Charge));
-                                        }
-                                    }
-
-                                    if (matchedFragmentIons.Count < CommonParameters.ScoreCutoff)
                                         continue;
-
-                                    // Score the peptide-spectrum match
-                                    double tic = theScan.TotalIonCurrent;
-                                    double score = 0;
-                                    foreach (var ion in matchedFragmentIons)
-                                    {
-                                        if (ion.NeutralTheoreticalProduct.ProductType != ProductType.D)
-                                        {
-                                            score += 1 + ion.Intensity / tic;
-                                        }
                                     }
 
-                                    var matchedIons = matchedFragmentIons.ToList(); // materialize before passing to another thread
-                                    AddPeptideCandidateToPsm(scan, score, specificBioPolymer, matchedIons);
+                                    // get the closest peak in the spectrum to the theoretical peak
+                                    var closestExperimentalMass = theScan.GetClosestExperimentalIsotopicEnvelope(product.NeutralMass);
+
+                                    // is the mass error acceptable?
+                                    if (closestExperimentalMass != null
+                                        && productTolerance.Within(closestExperimentalMass.MonoisotopicMass, product.NeutralMass)
+                                        && Math.Abs(closestExperimentalMass.Charge) <= Math.Abs(precursorCharge))//TODO apply this filter before picking the envelope
+                                    {
+                                        matchedFragmentIons.Add(new MatchedFragmentIon(product, closestExperimentalMass.MonoisotopicMass.ToMz(closestExperimentalMass.Charge),
+                                            closestExperimentalMass.Peaks.First().intensity, closestExperimentalMass.Charge));
+                                    }
                                 }
-                            }
 
-                            // report search progress (proteins searched so far out of total proteins in database)
-                            proteinsSearched++;
-                            var percentProgress = (int)((proteinsSearched / Proteins.Count) * 100);
+                                if (matchedFragmentIons.Count < CommonParameters.ScoreCutoff)
+                                    continue;
 
-                            if (percentProgress > oldPercentProgress)
-                            {
-                                oldPercentProgress = percentProgress;
-                                ReportProgress(new ProgressEventArgs(percentProgress, "Performing classic search... ", NestedIds));
+                                // Score the peptide-spectrum match
+                                double tic = theScan.TotalIonCurrent;
+                                double score = 0;
+                                foreach (var ion in matchedFragmentIons)
+                                {
+                                    if (ion.NeutralTheoreticalProduct.ProductType != ProductType.D)
+                                    {
+                                        score += 1 + ion.Intensity / tic;
+                                    }
+                                }
+
+                                var matchedIons = matchedFragmentIons.ToList(); // materialize before passing to another thread
+                                AddPeptideCandidateToPsm(scan, score, specificBioPolymer, matchedIons);
                             }
                         }
-                    });
+
+                        // report search progress (proteins searched so far out of total proteins in database)
+                        proteinsSearched++;
+                        var percentProgress = (int)((proteinsSearched / Proteins.Count) * 100);
+
+                        if (percentProgress > oldPercentProgress)
+                        {
+                            oldPercentProgress = percentProgress;
+                            ReportProgress(new ProgressEventArgs(percentProgress, "Performing classic search... ", NestedIds));
+                        }
+                    }
+                };
+
+                if (_singleThreadMode)
+                {
+                    processProteinRange(0, Proteins.Count);
+                }
+                else
+                {
+                    var proteinPartioner = Partitioner.Create(0, Proteins.Count);
+                    Parallel.ForEach(
+                        proteinPartioner,
+                        new ParallelOptions { MaxDegreeOfParallelism = CommonParameters.MaxThreadsToUsePerFile },
+                        (range, loopState) =>
+                        {
+                            processProteinRange(range.Item1, range.Item2);
+                        });
+                }
             }
 
             foreach (SpectralMatch psm in SpectralMatches.Where(p => p != null))
@@ -139,6 +156,35 @@ namespace EngineLayer.ClassicSearch
         private void AddPeptideCandidateToPsm(ScanWithIndexAndNotchInfo scan, double thisScore, IBioPolymerWithSetMods peptide, List<MatchedFragmentIon> matchedIons)
         {
             int scanIndex = scan.ScanIndex;
+
+            if (_singleThreadMode)
+            {
+                var existingPsm = SpectralMatches[scanIndex];
+
+                if (existingPsm != null)
+                {
+                    double scoreDiff = thisScore - existingPsm.RunnerUpScore;
+                    if (scoreDiff <= -SpectralMatch.ToleranceForScoreDifferentiation)
+                        return;
+                }
+
+                if (existingPsm == null)
+                {
+                    SpectralMatches[scanIndex] = GlobalVariables.AnalyteType == AnalyteType.Oligo
+                        ? new OligoSpectralMatch(peptide, scan.Notch, thisScore, scanIndex,
+                            ArrayOfSortedMS2Scans[scanIndex], CommonParameters, matchedIons)
+                        : new PeptideSpectralMatch(peptide, scan.Notch, thisScore, scanIndex,
+                            ArrayOfSortedMS2Scans[scanIndex], CommonParameters, matchedIons);
+                }
+                else
+                {
+                    existingPsm.AddOrReplace(peptide, thisScore, scan.Notch,
+                        CommonParameters.ReportAllAmbiguity, matchedIons);
+                }
+
+                return;
+            }
+
             var lockObj = Locks[scanIndex];
             // this is thread-safe because even if the score improves from another thread writing to this PSM,
             // the lock combined with AddOrReplace method will ensure thread safety
