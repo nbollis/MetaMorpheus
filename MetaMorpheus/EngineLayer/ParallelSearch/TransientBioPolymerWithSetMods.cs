0a9c99c9c9dba5666bef47b3683aaa09097b29c2
nbollis <nbollis@comcast.net>
2026-04-27 13:04:50 -0500

    refact(Move Transient Interface Implementors): More concrete now

--- MetaMorpheus/EngineLayer/ParallelSearch/TransientBioPolymerWithSetMods.cs
+++ MetaMorpheus/EngineLayer/ParallelSearch/TransientBioPolymerWithSetMods.cs
@@ -1,8 +1,6 @@
-﻿#nullable enable
-using Chemistry;
+﻿using EngineLayer.ClassicSearch;
 using MassSpectrometry;
 using Omics;
-using Omics.BioPolymer;
 using Omics.Digestion;
 using Omics.Fragmentation;
 using Omics.Modifications;
@@ -11,98 +9,14 @@ using Proteomics.ProteolyticDigestion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Chemistry;
 
-namespace EngineLayer.ClassicSearch;
-
-// Adaptor class to cache properties of IBioPolymer
-public class CachedBioPolymer : IBioPolymer
-{
-    private readonly IBioPolymer _bioPolymer;
-    public List<IBioPolymerWithSetMods>? DigestionProducts { get; private set; } = null;
-
-    public CachedBioPolymer(IBioPolymer bioPolymer)
-    {
-        _bioPolymer = bioPolymer;
-    }
-
-    public IEnumerable<IBioPolymerWithSetMods> Digest(IDigestionParams digestionParams, List<Modification> allKnownFixedModifications, List<Modification> variableModifications,
-        List<SilacLabel>? silacLabels = null, (SilacLabel startLabel, SilacLabel endLabel)? turnoverLabels = null,
-        bool topDownTruncationSearch = false)
-    {
-
-        if (DigestionProducts == null)
-        {
-            // Pre-cache the digestion products
-            DigestionProducts = _bioPolymer.Digest(digestionParams, allKnownFixedModifications, variableModifications, silacLabels, turnoverLabels, topDownTruncationSearch)
-                .Select(p => new CachedBioPolymerWithSetMods(p))
-                .Cast<IBioPolymerWithSetMods>()
-                .ToList();
-        }
-
-        return DigestionProducts;
-    }
-
-    #region Wrapped Properties
-    public string BaseSequence => _bioPolymer.BaseSequence;
-    public int Length => _bioPolymer.Length;
-    public string DatabaseFilePath => _bioPolymer.DatabaseFilePath;
-    public bool IsDecoy => _bioPolymer.IsDecoy;
-    public bool IsContaminant => _bioPolymer.IsContaminant;
-    public string Organism => _bioPolymer.Organism;
-    public string Accession => _bioPolymer.Accession;
-    public List<Tuple<string, string>> GeneNames => _bioPolymer.GeneNames;
-    public IDictionary<int, List<Modification>> OneBasedPossibleLocalizedModifications => _bioPolymer.OneBasedPossibleLocalizedModifications;
-    public string Name => _bioPolymer.Name;
-    public string FullName => _bioPolymer.FullName;
-    public string SampleNameForVariants => _bioPolymer.SampleNameForVariants;
-    public IDictionary<int, List<Modification>> OriginalNonVariantModifications
-    {
-        get => _bioPolymer.OriginalNonVariantModifications;
-        set => _bioPolymer.OriginalNonVariantModifications = value;
-    }
-    public IBioPolymer ConsensusVariant => _bioPolymer.ConsensusVariant;
-    public List<SequenceVariation> AppliedSequenceVariations => _bioPolymer.AppliedSequenceVariations;
-    public List<SequenceVariation> SequenceVariations => _bioPolymer.SequenceVariations;
-    public List<TruncationProduct> TruncationProducts => _bioPolymer.TruncationProducts;
-
-    #endregion
-
-    #region Wrapped Methods
-
-    public IBioPolymer CloneWithNewSequenceAndMods(string newBaseSequence, IDictionary<int, List<Modification>>? newMods) => new CachedBioPolymer(_bioPolymer.CloneWithNewSequenceAndMods(newBaseSequence, newMods));
-
-    public TBioPolymerType CreateVariant<TBioPolymerType>(string variantBaseSequence, TBioPolymerType original,
-        IEnumerable<SequenceVariation> appliedSequenceVariants, IEnumerable<TruncationProduct> applicableProteolysisProducts, IDictionary<int, List<Modification>> oneBasedModifications,
-        string sampleNameForVariants) where TBioPolymerType : IHasSequenceVariants 
-        => _bioPolymer.CreateVariant(variantBaseSequence, original, appliedSequenceVariants, applicableProteolysisProducts, oneBasedModifications, sampleNameForVariants);
-
-    public override bool Equals(object? obj)
-    {
-        // Unwrap if comparing to another CachedBioPolymer
-        if (obj is CachedBioPolymer cachedOther)
-            return _bioPolymer.Equals(cachedOther._bioPolymer);
-
-        // Direct comparison if comparing to raw IBioPolymer
-        if (obj is IBioPolymer protein)
-            return _bioPolymer.Equals(protein);
-
-        return false;
-    }
-
-    public override int GetHashCode()
-    {
-        return BaseSequence.GetHashCode();
-    }
-
-    #endregion
-}
-
-// adaptor
-public class CachedBioPolymerWithSetMods : IBioPolymerWithSetMods, IEquatable<PeptideWithSetModifications>, IEquatable<IBioPolymerWithSetMods>
+namespace EngineLayer.ParallelSearch;
+public class TransientBioPolymerWithSetMods : IBioPolymerWithSetMods, IEquatable<PeptideWithSetModifications>, IEquatable<IBioPolymerWithSetMods>
 {
     private readonly IBioPolymerWithSetMods _withSetMods;
     public List<Product>? TheoreticalFragments { get; private set; } = null;
-    public CachedBioPolymerWithSetMods(IBioPolymerWithSetMods withSetMods)
+    public TransientBioPolymerWithSetMods(IBioPolymerWithSetMods withSetMods)
     {
         _withSetMods = withSetMods;
     }
@@ -181,7 +95,7 @@ public class CachedBioPolymerWithSetMods : IBioPolymerWithSetMods, IEquatable<Pe
     // Funnel all equality checks through the IBioPolymerWithSetMods equality
     public override bool Equals(object? obj)
     {
-        if (obj is CachedBioPolymerWithSetMods cachedOther)
+        if (obj is TransientBioPolymerWithSetMods cachedOther)
             return Equals(cachedOther._withSetMods);
 
         if (obj is IBioPolymerWithSetMods pwsm)
